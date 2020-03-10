a2085d0b27dc5d1319685d64a9bfa2344b2a396b
VHS <listas.vhs@gmail.com>
2020-03-10 12:16:05 -0300

    Add a specific file to cross vars

---
+++ centreon_report_to_pdf/GlobalVars.py
@@ -0,0 +1,10 @@
+# Global variables and parameters
+csv_filepath = '/tmp/centreon.csv'
+report_type = 'x'
+report_type_name = 'x'
+final_HGs = [0]
+final_SGs = [0]
+pdf_output_file_path = None
+period = 'yesterday'
+custom_period_start = None
+custom_period_end = None
