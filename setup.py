25a1aa05d51d245ee64def42b152df5a1a8b3dd2
VHS <listas.vhs@gmail.com>
2020-05-25 12:12:17 -0400

    Add configurations files to pypi.org.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+ 
+setup(
+    name='centreon_report_to_pdf',
+    packages=['centreon_report_to_pdf'], 
+    version='1.4.0',
+    description='Generate a PDF from Centreon Dashboard and optional sent it by email',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Victor Hugo dos Santos',
+    author_email='listas.vhs@gmail.com',
+    url='https://github.com/vhsantos/centreon_report_to_pdf',
+    download_url='https://github.com/vhsantos/centreon_report_to_pdf/archive/v1.4.0.tar.gz', 
+    keywords='centreon report pdf email dashboard', 
+    classifiers = ["Programming Language :: Python :: 3.5",\
+        "Topic :: System :: Monitoring",\
+        "Development Status :: 6 - Mature", "Intended Audience :: System Administrators", \
+        "Operating System :: OS Independent", "Environment :: Console"],
+    install_requires=[
+        'reportlab>=3.5',
+        'pandas>=1.0',
+        'requests>=2.23',
+    ],
+    include_package_data = True
+)
